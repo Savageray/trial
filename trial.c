a0f82bbe0ecb8980f01ae53744cbd540a9c5ef24
spokenpool <rjwanyonyi@gmail.com>
2024-10-09 02:04:32 -0700

    first commit

---
+++ trial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int A,B,sum;
+printf("The Value of:  ");
+scanf("%d",& A);
+printf("The Value of:  ");
+scanf("%d",& B);
+sum=A+B;
+printf("The Sum of/n %d and %d = %d", A,B,sum);
+return 0;
+}
